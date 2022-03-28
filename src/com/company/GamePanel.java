ba23e16a141115a7738c39a3a622c2b246e2a0be
najeebislam <90645196+najeebislam@users.noreply.github.com>
2022-03-28 09:10:28 +0100

    Add files via upload
    NEW STUFF PROPER

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,14 +8,14 @@ public class GamePanel extends JPanel {
 
 
     //Screen Settings
-    final int originalTileSize = 32;  //32x32 tile
+    //final int originalTileSize = 32;  //32x32 tile
     final int scale = 2;
 
-    final int tileSize = originalTileSize * scale; //64x64 tile
+    //final int tileSize = originalTileSize * scale; //64x64 tile
     final int maxScreenCol = 32;
     final int maxScreenRow = 12;
-    final int screenWidth = 1600;
-    final int screenHeight = 900;
+    final int screenWidth = 1280;
+    final int screenHeight = 720;
 
     public GamePanel () {
 
@@ -22,6 +23,48 @@ public class GamePanel extends JPanel {
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
 
+        Icon buttonIcon = new ImageIcon("C:\\Users\\NI211044\\IdeaProjects\\2DGame1\\src\\com\\company\\grenade.png");
+        JButton grenadeButton = new JButton(buttonIcon);
+        grenadeButton.setSize(100,100);
+        grenadeButton.setBounds(540,350,250,250);
+        add(grenadeButton);
+        
+        
+        int Clicks = 0;
+        JLabel ClickCounter = new JLabel("Clicks" + 0);
+        ClickCounter.setSize(100,100);
+        ClickCounter.setBounds(540, 100, 300, 250);
+        add(ClickCounter);
+
+
+
+
+        setLayout(new BorderLayout());
+        JLabel background=new JLabel(new ImageIcon("C:\\Users\\NI211044\\IdeaProjects\\2DGame1\\src\\com\\company\\wallpaper_2.jpg"));
+        add(background);
+
+        background.setLayout(new FlowLayout());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //JFrame buttonFrame = new JFrame();
+        //Main.window.add(grenadeButton);
+        //Main.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Main.window.setLocationByPlatform(true);
+        //buttonFrame.setSize(500, 300);
+        //buttonFrame.setVisible(true);
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,60 +2,27 @@ package com.company;
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
-import com.company.gameObjects.*;
-import com.company.playerStats.*;
-import com.company.Automatic_Clickers.*;
-
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
-
-import static com.company.gameObjects.*;
 
 
 public class Main {
-
-
+    public static JFrame window = new JFrame();
     public static void main(String[] args) {
-        // creates timer task
-        /*
-        Timer t = new Timer();
-        TimerTask tt = new TimerTask() {
-            @Override
-            public void run() {
-                System.out.println("Task Timer on Fixed Rate");
-            };
-        };
-        t.scheduleAtFixedRate(tt,new Date(),1000);
-
-         */
 
 
-
-        ArrayList<Integer> AutomaticClickersOwned = new ArrayList<>();
-        ArrayList<Automatic_Clickers> AutomaticClickersList = new ArrayList<>();
-        playerStats player = new playerStats(0,0,0,AutomaticClickersOwned,1);
-
-        ArrayList<Automatic_Clickers> AutomaticClickerArray= CreateAutomaticClickers();
-        AutomaticClickersOwned = AmountOfAutomaticClickers(AutomaticClickerArray);
-        player = CreatePlayerObject(AutomaticClickersOwned);
-        System.out.println(player.toString());
-
-
-
-
-
-
-        JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(true);
         window.setTitle("Smash Those Explosives!");
 
+
+
+
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
+
+
+
+
 
         window.pack();
 
@@ -64,6 +31,8 @@ public class Main {
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+
     }
 }
 
